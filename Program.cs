3fad7dc0342240cd62ac4f85a4d393cc2641476b
bowcambia <william.forwarding@gmail.com>
2018-01-22 14:33:05 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportantGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print("Game has begun :)");
+        }
+
+        static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
