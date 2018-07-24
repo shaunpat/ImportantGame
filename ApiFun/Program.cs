aff6457518c04668e14657312ea80e1b71c1d5c2
wcam <campbell.programming@gmail.com>
2018-07-24 12:47:01 -0500

    Add files via upload

---
+++ ApiFun/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print("Hello and such :)");
+        }
+
+        public static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
