f14a4c8d8ec380865a272dbaefc748ab8820c935
wcam <campbell.programming@gmail.com>
2018-07-24 13:05:59 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ApiFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print("Hello and such :)");
+            
+            string ExecutablePath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            Print("The program executing right now is: " + ExecutablePath + "\\ApiFun.exe");
+
+            string InputPath = ConfigurationManager.AppSettings["InputFolder"];
+            Print("We are getting input from: " + InputPath);
+
+            string text = System.IO.File.ReadAllText(InputPath + "products.json");
+            Print("We found some text in our input! ");
+            Print(text);
+        }
+
+        public static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
